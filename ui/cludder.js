c592420195a2dc43ba87fb045125b6614fb8e6e3
timotree3 <timorcb@gmail.com>
2017-03-24 18:08:03 -0400

    broken state, but good changes

--- ui/cludder.js
+++ ui/cludder.js
@@ -1,27 +1,18 @@
 var Cludder = {posts:{},users:{},follows:{},nick:""};
 
-function send(fn,data,resultFn) {
-    $.post(
-        "/fn/cludder/"+fn,
-        data,
-        function(response) {
-            console.log("response: " + response);
-            resultFn(response);
-        }
-    ).error(function(response) {
-        console.log("response failed: " + response.responseText);
-    })
-    ;
+function send(fn,data) {
+    var result;
+    result = $.post("/fn/cludder/"+fn, data, function(v) {return v})
+    .error(function(response){console.log("response failed: " + response.responseText)})
+    console.log("result: " + result)
+    return result;
 };
 
 function getProfile() {
-    send("appProperty","App_Agent_Hash", function(me) {
-            send("getHandle",me,function(data) {
-                Cludder.nick = data;
-                $("#nick").html(data);
-                getMyPosts();
-        });
-    })
+    Cludder.me = send("appProperty", "App_Agent_Hash")
+    Cludder.nick = send("getHandle", Cludder.me);
+    $("#nick").html(Cludder.nick);
+    getMyPosts();
 }
 
 function addPost() {
@@ -30,22 +21,21 @@ function addPost() {
         message:$('#meow').val(),
         stamp: now.valueOf()
     };
-    send("post",post,function(data) {
-        post.key = data; // save the key of our post to the post
-        post.nick = Cludder.nick;
-        var id = cachePost(post);
-        $("#meows").prepend(makePostHTML(id,post,Cludder.nick));
-    });
+    data = send("post",post)
+    post.key = data; // save the key of our post to the post
+    post.nick = Cludder.nick;
+    id = post.stamp.toString()+nick;
+    Cludder.posts[id] = post;
+    $("#meows").prepend(makePostHTML(id,post,Cludder.nick));
 }
 
 function follow(w) {
     var follow = {
         whom:w
     };
-    send("follow",follow,function(data) {
-        follow.key = data; // save the key of our follow
-        var id = cacheFollow(follow);
-    });
+    data = send("follow",follow)
+    follow.key = data; // save the key of our follow
+    var id = cacheFollow(follow);
 }
 
 function makePostHTML(id,post) {
@@ -57,54 +47,40 @@ function makeUserHTML(user) {
 }
 
 function getMyPosts() {
-        send("appProperty", "App_Agent_Hash", function(me) {
-                getPosts(me)
-        })
-
+    getPosts(Cludder.me)
 }
 
 function getPosts(by) {
-    send("getPostsBy",by,function(arr) {
-        arr = JSON.parse(arr)
-        console.log("arr: " + JSON.stringify(arr))
-        for (var i = 0, len = arr.length; i < len; i++) {
-            console.log("arr[i]: " + JSON.stringify(arr[i]))
-            var post = arr[i].E.C;
-            post.nick = send("getHandle", by, function(author_handle) {
-                return author_handle;
-            })
-            var id = cachePost(post);
-            displayPosts();
-//            $("#meows").prepend(makePost(id,post));
-        }
-    });
+    posts_raw = send("getPostsBy",by)
+    console.log("posts: " + posts_raw)
+    posts = JSON.parse(posts_raw)
+    for (var i = 0, len = posts.length; i < len; i++) {
+        console.log("posts[i]: " + JSON.stringify(posts[i]))
+        var post = posts[i].E.C;
+        post.nick = send("getHandle", by)
+        id = post.stamp.toString()+nick;
+        Cludder.posts[id] = post;
+        displayPosts();
+    }
 }
 
 function getUsers() {
-    send("get",{what:"users"},function(arr) {
-        for (var i = 0, len = arr.length; i < len; i++) {
-            var user = JSON.parse(arr[i].C);
-            // don't cache yourself!
-            if (user.nick != Cludder.nick) {
-                cacheUser(user);
-            }
+    arr = send("get",{what:"users"})
+    for (var i = 0, len = arr.length; i < len; i++) {
+        var user = JSON.parse(arr[i].C);
+        // don't cache yourself!
+        if (user.nick != Cludder.nick) {
+            cacheUser(user);
         }
-    });
+    }
 }
 
 function getFollows(w) {
-    send("get",{what:"follows",whom:w},function(arr) {
-        for (var i = 0, len = arr.length; i < len; i++) {
-            var follow = JSON.parse(arr[i].C);
-            cacheFollow(follow);
-        }
-    });
-}
-
-function cachePost(p,nick) {
-    var id = p.stamp.toString()+nick;
-    Cludder.posts[id] = p;
-    return id;
+    arr = send("get",{what:"follows",whom:w})
+    for (var i = 0, len = arr.length; i < len; i++) {
+        var follow = JSON.parse(arr[i].C);
+        cacheFollow(follow);
+    }
 }
 
 function cacheUser(u) {
